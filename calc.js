8a4d04a9a775dc7583c8eb5a627bd2f8fb386101
tatyana-budnyk <tatyana.budnyk@gmail.com>
2017-03-26 10:46:48 +0300

    add js

--- calc.js
+++ calc.js
@@ -1,5 +1,5 @@
 var currentValue = document.forms.calculator.elements[0];
-var resultStr = "";
+var resultStr = 0;
 var operationFlag = false;
 var operationNumber;
 var operand1;
@@ -25,33 +25,30 @@ function insertNumber(x) {
 
 function calculateResult(x){
    var nullFlag = false;
-   switch(x) {
-        case 0: 
-                operationFlag = false;
-                switch(operationNumber){
-                    case 1: resultStr = (resultStr * 10 + operand1 * 10) / 10; 
-                    break;
-                    case 2: resultStr = (operand1 * 10 - resultStr * 10) / 10;
-                    break;
-                    case 3: resultStr = ((resultStr * 10) * (operand1 * 10)) / 100;
-                    break;
-                    case 4: if(resultStr == 0) {
-                            nullFlag = true;
-                            break;}
-                            resultStr = (operand1 * 10) / (resultStr * 10);
-                    break;}
-                operand1 = resultStr;
-                if (nullFlag) {currentValue.setAttribute("value", "Деление на ноль");}
-                else {currentValue.setAttribute("value", resultStr);}
-                break;
-        case 1: 
-        case 2: 
-        case 3:   
-        case 4: operationNumber = x; 
-                if (operationFlag) {break;}
-                operand1 = Number(resultStr);
-                resultStr = 0;
-                operationFlag = true;
-                break; 
-   }   
+   if (x === 0) {
+      operationFlag = false;
+      if (resultStr === "0") {resultStr = operand1;} 
+      switch(operationNumber){
+         case 1: resultStr = (resultStr * 10 + operand1 * 10) / 10; 
+         break;
+         case 2: resultStr = (operand1 * 10 - resultStr * 10) / 10;
+         break;
+         case 3: resultStr = ((resultStr * 10) * (operand1 * 10)) / 100;
+         break;
+         case 4: if(resultStr == 0) {
+                 nullFlag = true;
+                 break;}
+                 resultStr = (operand1 * 10) / (resultStr * 10);
+         break;}
+      operand1 = resultStr;
+      if (nullFlag) {currentValue.setAttribute("value", "Деление на ноль");}
+          else {currentValue.setAttribute("value", resultStr);}
+                }
+   else {
+        operationNumber = x; 
+        if (!operationFlag) { 
+           operand1 = Number(resultStr); //
+           resultStr = "0";
+           operationFlag = true;}
+        }
 } 
