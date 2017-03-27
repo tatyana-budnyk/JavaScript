ef16796e910844a63dc5ff621a7b644defbc2401
tatyana-budnyk <tatyana.budnyk@gmail.com>
2017-03-27 22:13:01 +0300

    add js

--- test.js
+++ test.js
@@ -1,14 +1,23 @@
 var resultButton = document.getElementById("result");
 var resultTest = 0;
+var checkBoxQuestion1 = 0;
+var checkBoxQuestion2 = 0;
 
 resultButton.onclick = function () {
       if (document.getElementsByName("question1")[0].checked) { resultTest += 20; }
       if (document.getElementsByName("question2")[2].checked) { resultTest += 20; }
       if (document.getElementsByName("question3")[3].checked) { resultTest += 20; }
-      if (document.getElementsByName("question4")[1].checked) { resultTest += 10; }
-      if (document.getElementsByName("question4")[2].checked) { resultTest += 10; }
-      if (document.getElementsByName("question5")[0].checked) { resultTest += 10; }
-      if (document.getElementsByName("question5")[1].checked) { resultTest += 10; }
+      if (document.getElementsByName("question4")[0].checked) { checkBoxQuestion1 -= 10; }
+      if (document.getElementsByName("question4")[1].checked) { checkBoxQuestion1 += 10; }
+      if (document.getElementsByName("question4")[2].checked) { checkBoxQuestion1 += 10; }
+      if (document.getElementsByName("question4")[3].checked) { checkBoxQuestion1 -= 10; }
+      if (checkBoxQuestion1 < 0) {checkBoxQuestion1 = 0;}
+      if (document.getElementsByName("question5")[0].checked) { checkBoxQuestion2 += 10; }
+      if (document.getElementsByName("question5")[1].checked) { checkBoxQuestion2 += 10; }
+      if (document.getElementsByName("question5")[2].checked) { checkBoxQuestion2 -= 10; }
+      if (document.getElementsByName("question5")[3].checked) { checkBoxQuestion2 -= 10; }
+      if (checkBoxQuestion2 < 0) {checkBoxQuestion2 = 0;}
+      resultTest += checkBoxQuestion1 + checkBoxQuestion2; 
       var messageElem = createMessage(resultTest);
       document.forms[0].appendChild(messageElem);
 }
